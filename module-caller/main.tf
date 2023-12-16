09178c72499786c21400916dbf9207cc7318e8f5
Andreadote <andre.mj@yahoo.com>
2023-12-16 05:59:00 -0600

    bug removed

--- module-caller/main.tf
+++ module-caller/main.tf
@@ -1,7 +1,3 @@
 module "s3" {
-<<<<<<< HEAD
-  source = "git::https://github.com/Andreadote/terraform-Tesla-s3.git//s3-module?ref=v1.1.0"
-=======
   source = "git::https://github.com/Andreadote/terraform-Tesla-s3.git//s3-module?ref=v1.2.0"
->>>>>>> fa465d8 ( fix: bugs removed)
 }
