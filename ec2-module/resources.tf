a4cf5d6b3f4f3f2f8f8eece09d1fb50646cf83aa
Andreadote <andre.mj@yahoo.com>
2024-01-16 17:59:39 -0600

    fix: updated ec2-module

---
+++ ec2-module/resources.tf
@@ -0,0 +1,46 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t3.micro"
+  monitoring           = true
+  ebs_optimized        = true
+  iam_instance_profile = "test"
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  root_block_device {
+    encrypted = true
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+resource "aws_network_interface" "foo" {
+  attachment {
+    instance     = aws_instance.web.id
+    device_index = 0
+  }
+}
