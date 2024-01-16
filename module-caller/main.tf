2d5767fba23b12ae951b9eb8aa6bc9b1f75631ed
Andreadote <andre.mj@yahoo.com>
2024-01-16 14:22:05 -0600

    fix:update_caller

--- module-caller/main.tf
+++ module-caller/main.tf
@@ -1,3 +1,3 @@
 module "s3" {
-  source = "git::https://github.com/Andreadote/terraform-Tesla-s3.git//s3-module?ref=v1.2.0"
+  source = "git::https://github.com/Andreadote/terraform-Tesla-s3.git//s3-module?ref=v1.5.2"
 }
