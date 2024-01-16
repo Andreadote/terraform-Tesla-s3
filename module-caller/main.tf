2e9746fc639860fcd1c424f0e6ca982737c6acd0
Andreadote <andre.mj@yahoo.com>
2024-01-16 15:26:53 -0600

    feat:backend add

--- module-caller/main.tf
+++ module-caller/main.tf
@@ -1,3 +1,12 @@
 module "s3" {
   source = "git::https://github.com/Andreadote/terraform-Tesla-s3.git//s3-module?ref=v1.5.2"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "elasticbeanstalk-ca-central-1-109753259968"
+    key    = "action/terraform.tfstate"
+    region = "ca-central-1"
+  }
+}
